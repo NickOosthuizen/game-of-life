49965ab72b2b5ebd143b49d32003b62d0bb4c7dc
Nick Oosthuizen <nickoosthuizen@g.ucla.edu>
2020-06-27 19:38:54 -0500

    Changed display implementation to a window with tkinter

--- game_of_life.py
+++ game_of_life.py
@@ -1,24 +1,49 @@
 import random
 import os
 import time
-# import tkinter
+import tkinter
 
-"""
+
 class Game_App(tkinter.Frame):
-    def __init__(self, master=None, initial_grid):
+    def __init__(self, master, initial_grid):
         super().__init__(master)
         self.master = master
-        self.grid_width = 50
-        self.grid_height = 38
-        self.pack()
-        self.add_grid(initial_grid)
+        self.grid_dim = initial_grid.grid_dim
+        # save a game_of_life object
+        self.game_grid = initial_grid
+        # array tracks canvas widgets
+        self.grid_widgets = []
+        self.add_grid()
 
-    def add_grid(self, grid):
-        self.grid_canvas = tkinter.Canvas(self.master, width = self.grid_width, height = seld.grid_height, bg= 'snow')
-        self.grid_canvas.pack()
-        img = tkinter.PhotoImage(width = self.grid_width, height = self.grid_height)
-"""
+    def add_grid(self):
+        size = self.grid_dim
+        for i in range(size):
+            grid_row = []
+            for j in range(size):
+                cell_color = 'white'
+                # seed widgets corresponding to living cells as black cells
+                if self.game_grid.grid[i][j] == 1:
+                    cell_color = 'black'
+                # each cell is a canvas widget 
+                square_canvas = tkinter.Canvas(self.master, width = 20, height = 20, bg = cell_color)
+                # widgets aligned in a square grid
+                square_canvas.grid(row = i, column = j)
+                grid_row.append(square_canvas)
+            self.grid_widgets.append(grid_row)
+    
+    def update_grid(self):
+        size = self.grid_dim
+        for i in range(size):
+            for j in range(size):
+                # update the colors of cells that have changed
+                if self.game_grid.grid[i][j] != self.game_grid.previous_grid[i][j]:
+                    cell_color = 'white'
+                    if self.game_grid.grid[i][j] == 1:
+                        cell_color = 'black'
+                    self.grid_widgets[i][j].config(bg = cell_color)
 
+
+                
 # game of life object to manage the grid of the program
 # follows the classic rules of Conway's Game of Life
 class game_of_life_grid():
@@ -85,19 +110,6 @@ class game_of_life_grid():
                 return False
         return True
 
-    # clears screen and prints current grid
-    def display_cells(self):
-        os.system('clear')
-        for row in self.grid:
-            display_row = ""
-            for cell in row:
-                if cell == 1:
-                    display_row += "@"
-                else:
-                    display_row += " "
-            print(display_row)
-        time.sleep(.5)
-
 
 def play_game():
     size = str()
@@ -110,12 +122,15 @@ def play_game():
         except ValueError:
             print("You have to pass in an integer!")
     cell_grid = game_of_life_grid(size)
-    cell_grid.display_cells()
+    root = tkinter.Tk()
+    app = Game_App(root, cell_grid)
     # run game until the grid reaches a static state
-    while cell_grid.grid_active():
-        cell_grid.play_round()
-        cell_grid.display_cells()
-
+    root.update()
+    while True:
+        app.game_grid.play_round()
+        app.update_grid()
+        root.update()
+        
 
 if __name__ == "__main__":
     play_game()
